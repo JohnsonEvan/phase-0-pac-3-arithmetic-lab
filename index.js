825b9ff89f8de95794ff7d44d82076a608c64598
Evan <evan.johnson99@yahoo.com>
2021-12-28 13:30:52 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(first,second){
+    return first + second;
+}
+function subtract(first, second){
+    return;
+}
+function multiply(first,second){
+    return;
+}
+function divide(first,second){
+    return;
+}
+function subtract(a,b){
+    return a - b ;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b ;
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+}
